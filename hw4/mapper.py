39c612b85cb964d024e6daf87db3fa65e5cb8e70
Shalini-Shukla <shalini.shukla1501@gmail.com>
2021-11-18 15:44:32 -0500

    HW4: Map Reduce for Temperature Data

---
+++ hw4/mapper.py
@@ -0,0 +1,9 @@
+import sys
+
+for line in sys.stdin:
+    line = line.strip()
+    temperature = int(line[87:92])
+    quality = int(line[92])
+    accepted_quality = [0,1,4,5,9]
+    if ((temperature != 9999) and quality in accepted_quality):
+        print('%s\t%d' % (line[15:23], int(line[87:92])))
